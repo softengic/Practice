504372f8a50863bbe28cbc9c321ee5ad8004a7e9
softengic <157764353+softengic@users.noreply.github.com>
2024-06-02 16:23:46 -0400

    coverted server to express

--- Week03_Tom/server.ts
+++ Week03_Tom/server.ts
@@ -1,36 +1,18 @@
-import http from 'http';  
-import fs from 'fs';
-import mime from 'mime-types';
+import express from 'express';
+
+const app = express();
+
 
 const port = 3000;
 
-let lookup = mime.lookup;
-
-const server = http.createServer(function(req, res)
+app.get('/', function(req, res) 
 {
-    let path = req.url as string;
-
-    if (path == "/" || path == "/home")
-    {
-        path = "/index.html";
-    };
-
-    let mime_type = lookup(path.substring(1)) as string;
-    console.log(`mime-type: ${mime_type}`);
-
-    fs.readFile(__dirname + path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end("ERROR: 404");
-        }
-
-        //res.setHeader("X-Content-Type-Options", "nosniff");
-        res.writeHead(200, {'Content-Type': mime_type});
-        
-        return res.end(data);
-    });
+    res.send('Hello World!');
 });
 
-server.listen(port, function () {
-    console.log(`Server Running at Port: ${port}`);
+
+//listener - similar to server-listen
+app.listen(port, function ()
+{
+    console.log(`Example app listening on port ${port}`);
 });
