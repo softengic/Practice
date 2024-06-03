2a04ae04a3a6efca79b54e89a207fe172eb8f32e
softengic <157764353+softengic@users.noreply.github.com>
2024-06-03 09:16:59 -0400

    commit from VSCode

---
+++ .history/Week03_Tom/server_20240602162117.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+
+const port = 3000;
+
+app.get('/', function(req, res) 
+{
+    res.send('Hello World!');
+});
+
+
+//listener - similar to server-listen
+app.listen(port, function ()
+{
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ .history/Week03_Tom/server_20240603085353.ts
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+import app from './app';
+import debug from 'debug';
+debug('Week03_Tom:server');
+import http from 'http';
+import { HttpError } from 'http-errors';
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val:string) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error:HttpError) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  let addr = server.address();
+  let bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
