2c9b1a8c642abe8394b9768304f15ead58c28e29
softengic <157764353+softengic@users.noreply.github.com>
2024-06-03 09:07:31 -0400

    convert project to typescript

--- Week03_Tom/server.ts
+++ Week03_Tom/server.ts
@@ -1,18 +1,73 @@
-import express from 'express';
+#!/usr/bin/env node
 
-const app = express();
+/**
+ * Module dependencies.
+ */
+
+import app from './app';
+import debug from 'debug';
+debug('Week03_Tom:server');
+import http from 'http';
+import { HttpError } from 'http-errors';
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 
-const port = 3000;
+const server = http.createServer(app);
 
-app.get('/', function(req, res) 
-{
-    res.send('Hello World!');
-});
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
-//listener - similar to server-listen
-app.listen(port, function ()
-{
-    console.log(`Example app listening on port ${port}`);
-});
+function normalizePort(val:string) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error:HttpError) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  let addr = server.address() as string;
+  let bind = 'pipe' + addr;
+  debug('Listening on ' + bind);
+}
