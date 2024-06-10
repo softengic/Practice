319608df89be452514d333ebffac31d7c4760e52
softengic <157764353+softengic@users.noreply.github.com>
2024-06-10 17:21:25 -0400

    added Movies model

---
+++ Week05_Tom/Server/Models/movie.ts
@@ -0,0 +1,22 @@
+//step 1: import mongoose
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+//step 2: create a Schema that matches the data
+const MovieSchema = new Schema
+    ({
+        Name: String,
+        Year: String,
+        Director: String,
+        Rating: String
+    },
+    {
+        collection: "movies"
+    }
+);
+
+//step 3: create a model using the Schema
+const Model = mongoose.model("Movies", MovieSchema);
+
+//step 4: export the modeln  => this makes the file a module
+export default Model;
