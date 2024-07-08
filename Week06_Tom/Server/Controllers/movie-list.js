9f257cd9f9d7f5f7f88f0f834c78117cce86a446
softengic <157764353+softengic@users.noreply.github.com>
2024-07-08 10:05:55 -0400

    configured the user to be detected

--- Week06_Tom/Server/Controllers/movie-list.js
+++ Week06_Tom/Server/Controllers/movie-list.js
@@ -5,10 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DisplayMovieList = void 0;
 const movie_1 = __importDefault(require("../Models/movie"));
+const Util_1 = require("../Util");
 function DisplayMovieList(req, res, next) {
     movie_1.default.find().exec()
         .then(moviesCollection => {
-        res.render('index', { title: 'Movie List', page: 'movie-list', displayName: '', movies: moviesCollection });
+        res.render('index', { title: 'Movie List', page: 'movie-list', displayName: (0, Util_1.UserDisplayName)(req), movies: moviesCollection });
     })
         .catch(err => {
         console.error(err);
